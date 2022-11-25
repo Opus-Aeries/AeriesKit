3f4bb39fb19e5eaa1b2b0b27a830b8dcbcc5ef5c
LeoSM_07 <leosmindlin@icloud.com>
2022-11-25 11:40:47 -0800

    Update

--- Sources/AeriesKit/Models/Grades/AKGradebook.swift
+++ Sources/AeriesKit/Models/Grades/AKGradebook.swift
@@ -1,15 +1,15 @@
 import Foundation
 
 public struct AKGradebook: AKData {
-    ///
+    /// The identifier number for this gradebook
     public var gradebookNumber: Int
-    ///
+    /// The name of the gradebook
     public var gradebookName: String
-    ///
+    /// The term identifier code that a gradebook belongs to
     public var termCode: String
-    ///
+    /// The name of the term that a gradebook belongs to
     public var termDescription: String
-    ///
+    /// The status of this gradebook
     public var status: String
     ///
     public var period: Int
@@ -24,36 +24,60 @@ public struct AKGradebook: AKData {
 
     public var assignments: [AKAssignment]
 
-    enum CodingKeys: CodingKey {
-        case gradebookNumber
-        case gradebookName
-        case termCode
-        case termDescription
-        case status
-        case period
-        case periodTitle
-        case startDate
-        case endDate
-        case categories
-        case assignments
+    enum CodingKeys: String, CodingKey {
+        case gradebookNumber = "GradebookNumber"
+        case gradebookName = "GradebookName"
+        case termCode = "TermCode"
+        case termDescription = "TermDescription"
+        case status = "Status"
+        case period = "Period"
+        case periodTitle = "PeriodTitle"
+        case startDate = "StartDate"
+        case endDate = "EndDate"
+        case categories = "Categories"
+        case assignments = "Assignments"
+    }
+
+    /// Create from data
+    public init(
+        gradebookNumber: Int,
+        gradebookName: String,
+        termCode: String,
+        termDescription: String,
+        status: String,
+        period: Int,
+        periodTitle: String,
+        startDate: String,
+        endDate: String,
+        categories: [AKAssignmentCategory],
+        assignments: [AKAssignment]
+    ) {
+        self.gradebookNumber = gradebookNumber
+        self.gradebookName = gradebookName
+        self.termCode = termCode
+        self.termDescription = termDescription
+        self.status = status
+        self.period = period
+        self.periodTitle = periodTitle
+        self.startDate = startDate
+        self.endDate = endDate
+        self.categories = categories
+        self.assignments = assignments
+    }
+
+    /// Decode from JSON
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.gradebookNumber = try container.decode(Int.self, forKey: .gradebookNumber)
+        self.gradebookName = try container.decode(String.self, forKey: .gradebookName)
+        self.termCode = try container.decode(String.self, forKey: .termCode)
+        self.termDescription = try container.decode(String.self, forKey: .termDescription)
+        self.status = try container.decode(String.self, forKey: .status)
+        self.period = try container.decode(Int.self, forKey: .period)
+        self.periodTitle = try container.decode(String.self, forKey: .periodTitle)
+        self.startDate = try container.decode(String.self, forKey: .startDate)
+        self.endDate = try container.decode(String.self, forKey: .endDate)
+        self.categories = try container.decode([AKAssignmentCategory].self, forKey: .categories)
+        self.assignments = try container.decode([AKAssignment].self, forKey: .assignments)
     }
 }
-
-/*
- "GradebookNumber": 5620689,
- "GradebookName": "AP English",
- "TermCode": "F",
- "TermDescription": "Fall",
- "Status": "Current",
- "Period": 5,
- "PeriodTitle": "5",
- "StartDate": "/Date(1660806000000)/",
- "EndDate": "/Date(1671177600000)/",
- "ShowWhatIf": true,
- "WhatIf": {
-     "Hide": false,
-     "Message": ""
- },
- "Categories": [],
- "Assignments": []
- */
